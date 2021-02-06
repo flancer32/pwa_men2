9af4c609fb9155b752ff79b4a1f304b930a21825
Alex Gusev <alex@flancer64.com>
2021-02-06 11:21:07 +0200

    WF-432 Add user session to Gen2 App

--- src/Defaults.mjs
+++ src/Defaults.mjs
@@ -8,4 +8,16 @@ export default class Fl32_Men2_Defaults {
     DI_VUE = 'core_vue';
     DI_VUE_ROUTER = 'core_vueRouter';
     DI_VUEX = 'core_vuex';
+    MOD_CORE;
+    MOD_USER;
+
+    constructor(spec) {
+        /** @type {TeqFw_Core_App_Defaults} */
+        const CORE = spec['TeqFw_Core_App_Defaults$'];    // instance singleton
+        this.MOD_CORE = CORE;
+        /** @type {Fl32_Teq_User_Defaults} */
+        const USER = spec['Fl32_Teq_User_Defaults$'];    // instance singleton
+        this.MOD_USER = USER;
+        Object.freeze(this);
+    }
 }
--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -1,19 +1,34 @@
 const template = `
 <div>
-    {{ out }}
+    <user-sign-in
+                  :data="signIn"
+                  @onSuccess="onSuccess($event)"
+                  @onFailure="onFailure($event)"
+    ></user-sign-in>
 </div>
 `;
 
 export default function Fl32_Men2_Front_App(spec) {
+    /** @type {Fl32_Teq_User_Front_Widget_SignIn} */
+    const userSignIn = spec['Fl32_Teq_User_Front_Widget_SignIn$'];  // instance singleton
 
     return {
         name: 'FrontApp',
         template,
-        components: {},
+        components: {userSignIn},
         data: function () {
             return {
                 out: 'Fl32_Men2_Front_App',
+                signIn: {},
             };
+        },
+        methods: {
+            onFailure(data) {
+                console.log('Failure');
+            },
+            async onSuccess() {
+                console.log('Success');
+            },
         },
     };
 }
