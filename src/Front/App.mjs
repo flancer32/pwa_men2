923452ba24c04e143604d818f88223bf8a1cd2d7
Alex Gusev <alex@flancer64.com>
2021-02-08 16:35:09 +0200

    WF-433 Improve handler for API requests

--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -5,7 +5,9 @@ const template = `
                   @onSuccess="onSuccess($event)"
                   @onFailure="onFailure($event)"
     ></user-sign-in>
-    <div v-show="isAuthenticated">Authenticated</div>
+    <div v-show="isAuthenticated">
+        <button v-on:click="signOut">Sign Out</button>
+    </div>
 </div>
 `;
 
@@ -16,6 +18,10 @@ export default function Fl32_Men2_Front_App(spec) {
     const session = spec[DEF.MOD_USER.DI_SESSION];  // named singleton
     /** @type {Fl32_Teq_User_Front_Widget_SignIn} */
     const userSignIn = spec['Fl32_Teq_User_Front_Widget_SignIn$'];  // instance singleton
+    /** @type {Fl32_Teq_User_Front_Gate_Sign_Out.gate} */
+    const gateSignOut = spec['Fl32_Teq_User_Front_Gate_Sign_Out$']; // function singleton
+    /** @type {typeof Fl32_Teq_User_Shared_Service_Route_Sign_Out_Request} */
+    const RequestSignOut = spec['Fl32_Teq_User_Shared_Service_Route_Sign_Out#Request']; // class constructor
     const mapMutations = spec[DEF.DI_VUEX].mapMutations;
     const mapState = spec[DEF.DI_VUEX].mapState;
 
@@ -38,13 +44,18 @@ export default function Fl32_Men2_Front_App(spec) {
             })
         },
         methods: {
-            onFailure(data) {
+            onFailure() {
                 console.log('Failure');
             },
             async onSuccess() {
                 // session is initiated in 'Fl32_Teq_User_Front_Widget_SignIn' before @success event.
                 const user = session.getUser();
                 this.setStateUserAuthenticated(user);
+            },
+            async signOut() {
+                const req = new RequestSignOut();
+                await gateSignOut(req);
+                this.setStateUserAuthenticated(null);
             },
             ...mapMutations({
                 setStateUserAuthenticated: 'user/setAuthenticated',
