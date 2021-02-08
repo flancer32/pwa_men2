a6e82273e3f52b6ee66f0c90eac403fd8d53d555
Alex Gusev <alex@flancer64.com>
2021-02-08 23:03:22 +0200

    WF-433 Improve handler for API requests

--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -7,6 +7,9 @@ const template = `
     ></user-sign-in>
     <div v-show="isAuthenticated">
         <button v-on:click="signOut">Sign Out</button>
+        <user-sign-up
+            :input="signUp"
+        ></user-sign-up>
     </div>
 </div>
 `;
@@ -17,22 +20,29 @@ export default function Fl32_Men2_Front_App(spec) {
     /** @type {Fl32_Teq_User_Front_App_Session} */
     const session = spec[DEF.MOD_USER.DI_SESSION];  // named singleton
     /** @type {Fl32_Teq_User_Front_Widget_SignIn} */
-    const userSignIn = spec['Fl32_Teq_User_Front_Widget_SignIn$'];  // instance singleton
+    const userSignIn = spec['Fl32_Teq_User_Front_Widget_SignIn$'];  // Vue component singleton
+    /** @type {Fl32_Teq_User_Front_Widget_SignUp} */
+    const userSignUp = spec['Fl32_Teq_User_Front_Widget_SignUp$'];  // Vue component singleton
     /** @type {Fl32_Teq_User_Front_Gate_Sign_Out.gate} */
     const gateSignOut = spec['Fl32_Teq_User_Front_Gate_Sign_Out$']; // function singleton
     /** @type {typeof Fl32_Teq_User_Shared_Service_Route_Sign_Out_Request} */
     const RequestSignOut = spec['Fl32_Teq_User_Shared_Service_Route_Sign_Out#Request']; // class constructor
+    /** @type {typeof Fl32_Teq_User_Front_Widget_SignIn_Props} */
+    const SignInProps = spec['Fl32_Teq_User_Front_Widget_SignIn#Props'];  // class constructor
+    /** @type {typeof Fl32_Teq_User_Front_Widget_SignUp_Props} */
+    const SignUpProps = spec['Fl32_Teq_User_Front_Widget_SignUp#Props'];  // class constructor
     const mapMutations = spec[DEF.DI_VUEX].mapMutations;
     const mapState = spec[DEF.DI_VUEX].mapState;
 
     return {
         name: 'FrontApp',
         template,
-        components: {userSignIn},
+        components: {userSignIn, userSignUp},
         data: function () {
             return {
                 out: 'Fl32_Men2_Front_App',
-                signIn: {},
+                signIn: new SignInProps(),
+                signUp: new SignUpProps(),
             };
         },
         computed: {
