6b7d3ad41dc2e4080aef93c036df18ed199d55e4
Alex Gusev <alex@flancer64.com>
2021-02-09 17:14:34 +0200

    WF-435 Improve handler hierarchy for HTTP2 server

--- bin/deploy/dev.sh
+++ bin/deploy/dev.sh
@@ -21,9 +21,7 @@ mkdir -p "${DIR_ROOT}/own_modules/@teqfw/"
 mkdir -p "${DIR_ROOT}/own_modules/@flancer32/"
 git clone https://github.com/teqfw/di.git "${DIR_ROOT}/own_modules/@teqfw/di"
 git clone https://github.com/teqfw/core-app.git "${DIR_ROOT}/own_modules/@teqfw/core-app"
-#git clone git@github.com:flancer32/teq_acl.git "${DIR_ROOT}/own_modules/@flancer32/teq_acl"
 git clone git@github.com:flancer32/teq_user.git "${DIR_ROOT}/own_modules/@flancer32/teq_user"
-#git clone https://github.com/flancer32/pwa_app.git "${DIR_ROOT}/own_modules/@flancer32/pwa_app"
 
 echo "Link dependencies to '/usr/lib/node_modules/'."
 cd "${DIR_ROOT}/own_modules/@teqfw/di" || exit 255
@@ -32,17 +30,11 @@ cd "${DIR_ROOT}/own_modules/@teqfw/core-app" || exit 255
 sudo npm link
 cd "${DIR_ROOT}/own_modules/@flancer32/teq_user" || exit 255
 sudo npm link
-#cd "${DIR_ROOT}/own_modules/@flancer32/teq_acl" || exit 255
-#sudo npm link
-#cd "${DIR_ROOT}/own_modules/@flancer32/pwa_app" || exit 255
-#sudo npm link
 
 echo "Link dependencies to the project."
 cd "${DIR_ROOT}" || exit 255
-npm link "@teqfw/di"
-npm link "@teqfw/core-app"
-npm link "@flancer32/teq_user"
-#npm link "@flancer32/teq_acl"
-#npm link "@flancer32/pwa_app"
+npm link "@teqfw/di" \
+  "@teqfw/core-app" \
+  "@flancer32/teq_user"
 
 echo "Done."
